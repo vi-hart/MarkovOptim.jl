f6904e8c2ac5c799f23a02104ced7e146f4a0216
Violet <pjh6654@rit.edu>
2021-09-18 22:14:10 -0400

    rate functions

--- src/MarkovOptim.jl
+++ src/MarkovOptim.jl
@@ -3,7 +3,9 @@ module MarkovOptim
 using Distributions
 
 export RandomParameter
+export RateFunction
 
 include("random_parameter.jl")
+include("rate_functions.jl")
 
 end
---
+++ src/rate_functions.jl
@@ -0,0 +1,21 @@
+abstract type AbstractRateFunction end
+
+struct RateFunction <: AbstractRateFunction
+    f::Function
+    npars::Int
+    shift::Bool
+end
+
+has_shift(RF::T) where T<:RateFunction = hasfield(T, :shift) && getfield(RF, :shift)
+get_function(RF::RateFunction) = getfield(RF, :f)
+@inline Base.length(RF::RateFunction) = RF.npars
+
+function (rate::RateFunction)(x::T, ps::Vector) where T
+    out = map(p->get_function(rate)(x, p), ps)
+    has_shift(rate) && pushfirst!(out, one(T))
+    return out
+end
+
+# Simple rate functions
+sigmoid = RateFunction((x, p) -> tanh((x + p[1])/p[2]), 2, true)
+linear = RateFunction((x, p) -> p[1]*x, 1, true)
