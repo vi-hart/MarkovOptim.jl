d7b66c09a7aff483555738286c422cd43466f2b8
Violet <pjh6654@rit.edu>
2021-09-19 17:36:09 -0400

    transition matrix generation added

--- src/MarkovOptim.jl
+++ src/MarkovOptim.jl
@@ -2,11 +2,13 @@ module MarkovOptim
 
 using Distributions
 using LightGraphs
+using LinearAlgebra
 
 export RandomParameter
 export RateFunction
 export MarkovModel
 
+include("utils.jl")
 include("random_parameter.jl")
 include("rate_functions.jl")
 include("markov.jl")
--- src/markov.jl
+++ src/markov.jl
@@ -8,6 +8,7 @@ mutable struct MarkovModel{T<:Integer, P<:Real} <: AbstractMarkovModel
     rate_params::RandomParameter{P}
     node_params::RandomParameter{P}
     edge_params::RandomParameter{P}
+    reduced_params::Union{Nothing, Matrix{P}}
 end
 
 function MarkovModel(
@@ -23,7 +24,7 @@ function MarkovModel(
     ratepars = RandomParameter(rate_params, terms - has_shift(rate_function), length(rate_function))
     nodepars = RandomParameter(node_params, nd, terms)
     edgepars = RandomParameter(edge_params, ed, terms)
-    return MarkovModel{T, P}(graph, open, terms, rate_function, ratepars, nodepars, edgepars)
+    return MarkovModel{T, P}(graph, open, terms, rate_function, ratepars, nodepars, edgepars, nothing)
 end
 
 function MarkovModel(
@@ -52,3 +53,49 @@ get_graph(M::MarkovModel) = getfield(M, :graph)
 rate_parameters(M::MarkovModel) = getfield(M, :rate_params)
 node_parameters(M::MarkovModel) = getfield(M, :node_params)
 edge_parameters(M::MarkovModel) = getfield(M, :edge_params)
+reduced_parameters(M::MarkovModel) = getfield(M, :reduced_params)
+is_reduced(M::T) where T<:MarkovModel = hasfield(T, :reduced_params) && !isnothing(M.reduced_params)
+
+rate_function(M::MarkovModel) = getfield(M, :rate_function)
+function rate_function(M::MarkovModel{T, P}, x::P) where {T, P}
+    ps = get_parameters(rate_parameters(M))
+    return rate_function(M)(x, ps)
+end
+
+function reduce_params(M::MarkovModel{T, P}) where {T, P}
+    graph = get_graph(M)
+    ic = _incidence(graph)
+    α = node_parameters(M)
+    β = edge_parameters(M)
+    E = ne(graph)
+    D = Matrix{P}(undef, E, 2E)
+    D[:,1:2:end] .= I(E)
+    D[:,2:2:end] .= -I(E)
+    return inv([D;abs.(D)])*[ic'α;β]
+end
+
+function reduce_params!(M::MarkovModel)
+    theta = reduce_params(M)
+    setfield!(M, :reduced_params, theta)
+    return M
+end
+
+function rates(M::MarkovModel{T, P}, x::P) where {T, P}
+    !is_reduced(M) && reduce_params!(M)
+    theta = reduced_parameters(M)
+    return exp.(theta * rate_function(M, x))
+end
+
+function transition_matrix(M::MarkovModel{T, P}, x::P) where {T, P}
+    r = rates(M, x)
+    dic = _directed_incidence(get_graph(M))
+    return dic*Diagonal(r[:])*(-min.(dic, 0)')
+end
+Base.Matrix(M::MarkovModel{T, P}, x::P) where {T, P} = transition_matrix(M, x)
+
+function steady_states(M::MarkovModel{T, P}, x::P) where {T, P}
+    rates = rate_function(M, x)
+    α = node_parameters(M)
+    s = [exp(dot(rates, α[i,:])) for i in 1:size(α, 1)]
+    return s /= sum(s)
+end
